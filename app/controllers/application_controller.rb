f2e4e790c028e4aee02f2f2d0542734969ddf7b7
RodrigoGOP <rodrigo.gonzalez@chicagobooth.edu>
2020-10-15 04:07:45 +0000

    Random working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
 
 def blank_square_form
-
   render ({ :template => "calculation_templates/square_form.html.erb"})
-  
 end
 
 def calculate_square
@@ -13,7 +11,20 @@ def calculate_square
   @square_of_num = @num ** 2
 
   render ({ :template => "calculation_templates/square_results.html.erb"})
-  
+end
+
+def blank_random_form
+  render ({ :template => "calculation_templates/random_form.html.erb"})
+end
+
+def calculate_random
+  #params = {"elephant"=>"42"}
+
+@lower = params.fetch("user_min").to_f
+@upper = params.fetch("user_max").to_f
+@result = rand(@lower..@upper)
+
+  render ({ :template => "calculation_templates/random_results.html.erb"})
 end
 
 end
