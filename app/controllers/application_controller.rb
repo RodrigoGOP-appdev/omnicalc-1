583af7589aa7ce54442d75e7fd8e33c4521f7dcf
RodrigoGOP <rodrigo.gonzalez@chicagobooth.edu>
2020-10-15 03:38:48 +0000

    Square totally working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ def blank_square_form
   
 end
 
+def calculate_square
+  #params = {"elephant"=>"42"}
+
+  @num = params.fetch("elephant").to_f
+  @square_of_num = @num ** 2
+
+  render ({ :template => "calculation_templates/square_results.html.erb"})
+  
+end
+
 end
